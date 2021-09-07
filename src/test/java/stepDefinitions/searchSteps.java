b77a6069daf00afc2d16844458dc22d97b5b8915
Diana Gutierrez Contreras <diana.gutierrezc@perficient.com>
2021-09-07 16:18:51 -0500

    remove system.out.println

--- src/test/java/stepDefinitions/searchSteps.java
+++ src/test/java/stepDefinitions/searchSteps.java
@@ -14,7 +14,6 @@ public class searchSteps {
 
 	WebDriver driver = null;
 
-
 	@Given("browser is open")
 	public void browser_is_open() {
 
@@ -26,47 +25,39 @@ public class searchSteps {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
-		System.out.println("Browser is open");
-
 	}
-	
+
 	@And("go to google page")
 	public void go_to_google_page() {
 
 		driver.navigate().to("https://www.google.com");
-		System.out.println("Inside google");
+
 	}
 
-	
 	@When("user send word to search")
 	public void user_send_automation_to_search() {
 
 		driver.findElement(By.name("q")).sendKeys("automation");
-		System.out.println("send search word");
 	}
 
 	@And("execute enter key")
 	public void execute_enter_key() {
 
 		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-		System.out.println("user enter key");
 	}
-	
-	
+
 	@Then("user see the results")
 	public void user_see_the_results() {
 
 		driver.getPageSource().contains("Automatización industrial - Wikipedia, la enciclopedia libre");
 
-		System.out.println("The results are displayed");
 	}
-	
+
 	@After
 	public void quit() {
-		
+
 		driver.close();
 		driver.quit();
-		System.out.println("Close driver");
 	}
 
 }
