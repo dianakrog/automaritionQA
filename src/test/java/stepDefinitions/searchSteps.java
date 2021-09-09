06a2f5692f87bbabb65d941294c30511d8ffa49e
Diana Gutierrez Contreras <diana.gutierrezc@perficient.com>
2021-09-09 14:47:08 -0500

    fix drivers

--- src/test/java/stepDefinitions/searchSteps.java
+++ src/test/java/stepDefinitions/searchSteps.java
@@ -3,47 +3,52 @@ package stepDefinitions;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import io.cucumber.java.After;
 import io.cucumber.java.en.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class searchSteps {
 
-	
-	WebDriver driver;
-	
-	WebDriverManager wdmC = WebDriverManager.chromedriver();
-	WebDriverManager wdmF = WebDriverManager.firefoxdriver();
+	WebDriver driver = null;
 
+		WebDriverManager wdmC = WebDriverManager.chromedriver();
+		WebDriverManager wdmF = WebDriverManager.firefoxdriver();
+		
+		//WebDriverManager.chromedriver().setup();
+		//WebDriverManager.firefoxdriver().setup();
 
 	@Given("{string} is open")
 	public void browser_is_open(String browser) {
 
-		//String projectPath = System.getProperty("user.dir");
+		// String projectPath = System.getProperty("user.dir");
 
 		switch (browser) {
 		case "chrome":
-			
-			 ChromeOptions optionsC = new ChromeOptions();
 
-			//System.setProperty("webdriver.chrome.driver", projectPath + "/src/test/resources/drivers/chromedriver.exe");
+			ChromeOptions optionsC = new ChromeOptions();
+
+			// System.setProperty("webdriver.chrome.driver", projectPath +
+			// "/src/test/resources/drivers/chromedriver.exe");
 			//driver = new ChromeDriver();
 			driver = wdmC.capabilities(optionsC).create();
 			break;
 
 		case "firefox":
-			
+
 			FirefoxOptions optionsF = new FirefoxOptions();
-			//System.setProperty("webdriver.gecko.driver", projectPath + "/src/test/resources/drivers/geckodriver.exe");
+			// System.setProperty("webdriver.gecko.driver", projectPath +
+			// "/src/test/resources/drivers/geckodriver.exe");
 			//driver = new FirefoxDriver();
 			driver = wdmF.capabilities(optionsF).create();
-			//optionsF.addArguments("--headless");
+			// optionsF.addArguments("--headless");
 			break;
 
 		}
@@ -69,8 +74,8 @@ public class searchSteps {
 	public void execute_enter_key() throws InterruptedException {
 
 		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-		TimeUnit.SECONDS.sleep(5);
-		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		TimeUnit.SECONDS.sleep(3);
+		// driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@Then("user see the results")
